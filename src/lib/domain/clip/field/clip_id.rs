e63604495d9c178e2ae95adb20dc0b8cca5ff4b1
petrostrak <pit.trak@gmail.com>
2023-03-30 17:19:15 +0300

    Add clipid struct and its impl

--- src/lib/domain/clip/field/clip_id.rs
+++ src/lib/domain/clip/field/clip_id.rs
@@ -1,1 +1,23 @@
+use crate::data::Dbid;
+use derive_more::Constructor;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Constructor, Serialize, Deserialize)]
+pub struct ClipId(Dbid);
+impl ClipId {
+    pub fn into_inner(self) -> Dbid {
+        self.0
+    }
+}
+
+impl From<Dbid> for ClipId {
+    fn from(value: Dbid) -> Self {
+        Self(value)
+    }
+}
+
+impl Default for ClipId {
+    fn default() -> Self {
+        Self(Dbid::nil())
+    }
+}
