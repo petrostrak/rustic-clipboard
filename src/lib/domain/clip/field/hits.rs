bfb5767fbd7bb473b932a81f968985a9c156cad8
petrostrak <pit.trak@gmail.com>
2023-03-30 16:18:47 +0300

    Add hits struct and basic impl

--- src/lib/domain/clip/field/hits.rs
+++ src/lib/domain/clip/field/hits.rs
@@ -1,1 +1,10 @@
+use derive_more::Constructor;
+use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Constructor, Debug, Serialize, Deserialize)]
+pub struct Hits(u64);
+impl Hits {
+    pub fn into_inner(self) -> u64 {
+        self.0
+    }
+}
