704c0534164ecac4d7339a4023fa32c1a0c9be9b
petrostrak <pit.trak@gmail.com>
2023-03-30 16:42:39 +0300

    Add shortcode and several impls

--- src/lib/domain/clip/field/shortcode.rs
+++ src/lib/domain/clip/field/shortcode.rs
@@ -1,1 +1,59 @@
+use std::str::FromStr;
 
+use super::ClipError;
+use derive_more::From;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, From)]
+pub struct ShortCode(String);
+impl ShortCode {
+    pub fn new() -> Self {
+        use rand::prelude::*;
+
+        let allowed_chars = ['a', 'b', 'c', 'd', '1', '2', '3', '4'];
+
+        let mut rng = thread_rng();
+        let mut shortcode = String::with_capacity(10);
+        for _ in 0..10 {
+            shortcode.push(
+                *allowed_chars
+                    .choose(&mut rng)
+                    .expect("sampling array should have values"),
+            );
+        }
+        Self(shortcode)
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+}
+
+impl Default for ShortCode {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl From<ShortCode> for String {
+    fn from(value: ShortCode) -> Self {
+        value.0
+    }
+}
+
+impl From<&str> for ShortCode {
+    fn from(value: &str) -> Self {
+        ShortCode(value.to_owned())
+    }
+}
+
+impl FromStr for ShortCode {
+    type Err = ClipError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.into()))
+    }
+}
