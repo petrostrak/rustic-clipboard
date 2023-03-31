0ccbd653b8d50a91ab0cf095d65299bb4c215705
petrostrak <pit.trak@gmail.com>
2023-03-31 19:09:18 +0300

    Add model clip and impl TryFrom for relational mapping

---
+++ src/lib/data/model.rs
@@ -0,0 +1,37 @@
+use crate::data::Dbid;
+use crate::{ClipError, Time};
+use chrono::NaiveDateTime;
+use std::convert::TryFrom;
+use std::str::FromStr;
+
+// model::Clip encapsulates DB specific information
+// and is used only when interacting with the DB
+#[derive(Debug, sqlx::FromRow)]
+pub struct Clip {
+    pub(in crate::data) clip_id: String,
+    pub(in crate::data) shortcode: String,
+    pub(in crate::data) content: String,
+    pub(in crate::data) title: Option<String>,
+    pub(in crate::data) posted: NaiveDateTime,
+    pub(in crate::data) expires: Option<NaiveDateTime>,
+    pub(in crate::data) password: Option<String>,
+    pub(in crate::data) hits: i64,
+}
+
+// Converting model::Clip into domain::Clip
+impl TryFrom<Clip> for crate::domain::Clip {
+    type Error = ClipError;
+    fn try_from(value: Clip) -> Result<Self, Self::Error> {
+        use crate::domain::clip::field;
+        Ok(Self {
+            clip_id: field::ClipId::new(Dbid::from_str(value.clip_id.as_str())?),
+            shortcode: field::ShortCode::from(value.shortcode),
+            content: field::Content::new(value.content.as_str())?,
+            title: field::Title::new(value.title),
+            posted: field::Posted::new(Time::from_naive_utc(value.posted)),
+            expires: field::Expires::new(value.expires.map(Time::from_naive_utc)),
+            password: field::Password::new(value.password.unwrap_or_default())?,
+            hits: field::Hits::new(u64::try_from(value.hits)?),
+        })
+    }
+}
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -2,3 +2,7 @@ pub mod data;
 pub mod domain;
 pub mod service;
 pub mod web;
+
+pub use domain::clip::field::ShortCode; // Is utilized by the API server and the domain mods
+pub use domain::clip::ClipError;
+pub use domain::time::Time;
