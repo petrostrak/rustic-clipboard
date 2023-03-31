019aa00b35fce7d8c46f813a4712ce98cb7338f3
petrostrak <pit.trak@gmail.com>
2023-03-31 18:33:05 +0300

    Add db aliases

--- src/lib/data/mod.rs
+++ src/lib/data/mod.rs
@@ -1,7 +1,20 @@
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::{Database, Sqlite};
 use std::str::FromStr;
 use uuid::Uuid;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error),
+}
+
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::SqlitePool;
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>;
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
 
 #[derive(Debug, Clone, From, Display, Serialize, Deserialize)]
 pub struct Dbid(Uuid);
