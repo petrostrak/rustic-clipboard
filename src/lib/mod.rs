d06805d47458a617136886b54b709e7b31b63de2
petrostrak <pit.trak@gmail.com>
2023-04-03 18:24:32 +0300

    Add service error enum

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -3,6 +3,7 @@ pub mod domain;
 pub mod service;
 pub mod web;
 
+pub use data::DataError;
 pub use domain::clip::field::ShortCode; // Is utilized by the API server and the domain mods
 pub use domain::clip::ClipError;
 pub use domain::time::Time;
