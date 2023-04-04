c53782f0e67861ec4090eefec306d162e16c93a9
petrostrak <pit.trak@gmail.com>
2023-04-04 18:21:49 +0300

    Add rocket builder

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -3,8 +3,26 @@ pub mod domain;
 pub mod service;
 pub mod web;
 
+use data::AppDatabase;
 pub use data::DataError;
 pub use domain::clip::field::ShortCode; // Is utilized by the API server and the domain mods
 pub use domain::clip::{Clip, ClipError};
 pub use domain::time::Time;
+use rocket::fs::FileServer;
+use rocket::{Build, Rocket};
 pub use service::ServiceError;
+use web::renderer::Renderer;
+
+pub fn rocket(config: RocketConfig) -> Rocket<Build> {
+    rocket::build()
+        .manage::<AppDatabase>(config.database)
+        .manage::<Renderer>(config.renderer)
+        .mount("/", web::http::routes())
+        .mount("/static", FileServer::from("static"))
+        .register("/", web::http::catcher::catchers())
+}
+
+pub struct RocketConfig {
+    pub renderer: Renderer<'static>,
+    pub database: AppDatabase,
+}
