2c86dea06d240d9bd7eda864b211398fd1c64dcc
petrostrak <pit.trak@gmail.com>
2023-04-04 15:12:02 +0300

    Add rocket framework and PageError enum

--- src/lib/web/mod.rs
+++ src/lib/web/mod.rs
@@ -1,2 +1,26 @@
 pub mod ctx;
 pub mod renderer;
+
+#[derive(rocket::Responder)]
+pub enum PageError {
+    #[response(status = 500)]
+    Serialization(String),
+    #[response(status = 500)]
+    Render(String),
+    #[response(status = 404)]
+    NotFound(String),
+    #[response(status = 500)]
+    Internal(String),
+}
+
+impl From<handlebars::RenderError> for PageError {
+    fn from(value: handlebars::RenderError) -> Self {
+        PageError::Render(format!("{}", value))
+    }
+}
+
+impl From<serde_json::Error> for PageError {
+    fn from(value: serde_json::Error) -> Self {
+        PageError::Serialization(format!("{}", value))
+    }
+}
