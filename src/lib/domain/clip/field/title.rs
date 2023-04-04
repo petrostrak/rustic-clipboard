b458602e1f1e10b17ade90a93b5f45f761b98438
petrostrak <pit.trak@gmail.com>
2023-04-04 17:43:30 +0300

    Implement fromformfield trait for form structs

--- src/lib/domain/clip/field/title.rs
+++ src/lib/domain/clip/field/title.rs
@@ -1,7 +1,8 @@
+use rocket::form::{self, FromFormField, ValueField};
+use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
 use crate::domain::clip::ClipError;
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Title(Option<String>);
@@ -35,5 +36,12 @@ impl FromStr for Title {
     type Err = ClipError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Self::new(s.to_string()))
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromFormField<'r> for Title {
+    fn from_value(field: ValueField<'r>) -> form::Result<'r, Self> {
+        Ok(Self::new(field.value.to_owned()))
     }
 }
