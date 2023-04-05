5befbd9d05abba8a1dd6d1097c3151b214b6c805
petrostrak <pit.trak@gmail.com>
2023-04-05 11:46:24 +0300

    add password protected clip logic

--- src/lib/web/http.rs
+++ src/lib/web/http.rs
@@ -9,6 +9,8 @@ use rocket::response::content::RawHtml;
 use rocket::response::{status, Redirect};
 use rocket::{uri, State};
 
+use super::PASSWORD_COOKIE;
+
 #[rocket::get("/")]
 fn home(renderer: &State<Renderer<'_>>) -> RawHtml<String> {
     let contecx = ctx::Home::default();
@@ -96,8 +98,48 @@ pub async fn get_clip(
     }
 }
 
+#[rocket::post("/clip/<shortcode>", data = "<form>")]
+pub async fn submit_clip_password(
+    cookies: &CookieJar<'_>,
+    shortcode: ShortCode,
+    form: Form<Contextual<'_, form::GetPasswordProtectedClip>>,
+    database: &State<AppDatabase>,
+    renderer: &State<Renderer<'_>>,
+) -> Result<RawHtml<String>, PageError> {
+    if let Some(form) = &form.value {
+        let req = service::ask::GetClip {
+            shortcode: shortcode.clone(),
+            password: form.password.clone(),
+        };
+        match action::get_clip(req, database.get_pool()).await {
+            Ok(clip) => {
+                let context = ctx::ViewClip::new(clip);
+                cookies.add(Cookie::new(
+                    PASSWORD_COOKIE,
+                    form.password.clone().into_inner().unwrap_or_default(),
+                ));
+                Ok(RawHtml(renderer.render(context, &[])))
+            }
+            Err(e) => match e {
+                ServiceError::PermissionError(e) => {
+                    let context = ctx::PasswordRequired::new(shortcode);
+                    Ok(RawHtml(renderer.render(context, &[e.as_str()])))
+                }
+                ServiceError::NotFound => Err(PageError::NotFound("Clip not found".to_owned())),
+                _ => Err(PageError::Internal("Internal server error".to_owned())),
+            },
+        }
+    } else {
+        let context = ctx::PasswordRequired::new(shortcode);
+        Ok(RawHtml(renderer.render(
+            context,
+            &["A password required to view this clip"],
+        )))
+    }
+}
+
 pub fn routes() -> Vec<rocket::Route> {
-    rocket::routes![home, get_clip, new_clip]
+    rocket::routes![home, get_clip, new_clip, submit_clip_password]
 }
 
 pub mod catcher {
